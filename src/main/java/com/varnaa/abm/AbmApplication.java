d6ffe7d838bea2f7ccb05027288dc5ef77026b33
varnaa <swersie723>
2020-07-25 17:54:19 +0530

    inital commit.

---
+++ src/main/java/com/varnaa/abm/AbmApplication.java
@@ -0,0 +1,13 @@
+package com.varnaa.abm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/varnaa/abm/AbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varnaa.abm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
